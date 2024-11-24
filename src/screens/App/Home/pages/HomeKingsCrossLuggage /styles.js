ea9270bc6c5cb5639423c258648ee455d68f59bf
sarujan1 <sarujan@codelantic.com>
2024-11-24 17:48:15 +0530

    update

--- src/screens/App/Home/pages/HomeKingsCrossLuggage /styles.js
+++ src/screens/App/Home/pages/HomeKingsCrossLuggage /styles.js
@@ -26,7 +26,7 @@ export const styles = StyleSheet.create({
     },
     cameraView: {
         width: '100%',
-        height:'100%',
+        height: '100%',
         borderRadius: 10,
     },
     tabButton: {
